e8a8c2d4afffe417806dee087a3bb2a8d93af9f1
Your Name <essam.mohamed.dev@gmail.com>
2024-12-25 19:27:23 +0200

    Update Footer.jsx

--- components/layout/footer/Footer.jsx
+++ components/layout/footer/Footer.jsx
@@ -5,21 +5,21 @@ const Footer = () => {
   return (
     <footer className="bg-bg_alt py-8">
       <div className="container">
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col justify-between items-center md:flex-col">
           <div className="text-primary_color">
             <h1>Copyright © 2024 Essam Mohamed. All Rights Reserves</h1>
           </div>
           <div className="flex gap-[10px] items-center">
-              {INFO_LINKS.map((item) => (
-                <Link
-                  key={item.id}
-                  href={item.path}
-                  className="text-secondary_color text-[15px] px-[12px] py-[10px] rounded-[10px] hover:bg-main_color"
-                >
-                  {item.icon}
-                </Link>
-              ))}
-            </div>
+            {INFO_LINKS.map((item) => (
+              <Link
+                key={item.id}
+                href={item.path}
+                className="text-secondary_color text-[15px] px-[12px] py-[10px] rounded-[10px] hover:bg-main_color"
+              >
+                {item.icon}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </footer>
